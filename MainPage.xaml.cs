f33d36e58b158cc8917fef51215dd30b75a4b206
lebao3105 <bao12345yocoo@gmail.com>
2022-12-11 22:39:51 +0700

    WinUI version

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -11,20 +13,77 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace UWPQuickshortcuts
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+            NavigateToView("HomePage");
+        }
+
+        private NavigationViewItem _lastitem;
+
+        private bool NavigateToView(string clickedView)
+        {
+            var view = Assembly.GetExecutingAssembly()
+                .GetType($"UWPQuickshortcuts.Views.{clickedView}");
+
+            if (string.IsNullOrWhiteSpace(clickedView) || view == null)
+            {
+                return false;
+            }
+            ContentFm.Navigate(view, null, new EntranceNavigationTransitionInfo());
+            return true;
+        }
+
+        private async void navView_ItemInvoked(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked)
+            {
+                UWPQuickshortcuts.Views.WindowsSettings dlg = new UWPQuickshortcuts.Views.WindowsSettings();
+                await dlg.ShowAsync();
+                return;
+            }
+
+            var item = args.InvokedItem as NavigationViewItem;
+            if (item == null || item == _lastitem)
+                return;
+            var clickedView = item.Tag?.ToString();
+            Debug.WriteLine(clickedView);
+            if (clickedView == "Power Options")
+            {
+                UWPQuickshortcuts.Views.PowerOpts dlg = new UWPQuickshortcuts.Views.PowerOpts();
+                await dlg.ShowAsync();
+            }
+            else
+            {
+                if (!NavigateToView(clickedView)) return;
+            }
+            _lastitem = item;
+        }
+
+        private void navView_BackRequested(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewBackRequestedEventArgs args)
+        {
+            if (ContentFm.CanGoBack)
+                ContentFm.GoBack();
+        }
+
+        private async void ContentFm_NavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            var dlg = new ContentDialog
+            {
+                Title = "Error",
+                Content = "Unable to switch between pages via the hamburger menu.\nPlease contact the developer.",
+                CloseButtonText = "OK"
+            };
+            await dlg.ShowAsync();
         }
     }
 }
---
+++ Views/WindowsSettings.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UWPQuickshortcuts.Views
+{
+    public sealed partial class WindowsSettings : ContentDialog
+    {
+        public WindowsSettings()
+        {
+            this.InitializeComponent();
+        }
+
+        private string selected_item;
+        private string cmd;
+
+        private async void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            switch (selected_item)
+            {
+                case "Sign-in options":
+                    cmd = "signinoptions";
+                    break;
+                case "Your info":
+                    cmd = "yourinfo";
+                    break;
+                case "Apps and Features":
+                    cmd = "appsfeatures";
+                    break;
+                case "Default apps":
+                    cmd = "defaultapps";
+                    break;
+                case "Bluetooth":
+                    cmd = "bluetooth";
+                    break;
+                case "Camera":
+                    cmd = "camera";
+                    break;
+                case "Mouse (and touchpad)":
+                    cmd = "mousetouchpad";
+                    break;
+                case "USB":
+                    cmd = "usb";
+                    break;
+                case "Wifi":
+                    cmd = "network-wifi";
+                    break;
+                case "Airplane mode":
+                    cmd = "proximity";
+                    break;
+                case "Cellular":
+                    cmd = "network-cellular";
+                    break;
+                case "Mobile hotspot":
+                    cmd = "network-mobilehotpost";
+                    break;
+            }
+            if (selected_item == "Settings") {
+                await launcher.OpenProtocol("ms-settings://", "Windows Settings");
+                return;
+            }
+            await launcher.OpenProtocol("ms-settings://" + cmd, selected_item + " on Settings");
+        }
+
+        private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            // do nothing
+        }
+
+        private void ItemsCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selected_item = (string)e.AddedItems[0];
+        }
+    }
+}
